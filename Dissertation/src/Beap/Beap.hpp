a4c284e310687b47722b8a141200dd4a1e03001f
Lawal Alongbija <lawal@Lawals-MacBook-Pro.local>
2025-11-12 20:55:55 +0000

    another junction

--- Dissertation/src/Beap/Beap.hpp
+++ Dissertation/src/Beap/Beap.hpp
@@ -1,13 +1,14 @@
 #include <vector>
 #include <array>
+#include <iostream>
 
 
 class Beap
 {
 public:
 	std::vector<int> container;
-	unsigned int size;
-	unsigned int height;
+	int size;
+	int height;
 
 	Beap() {
 		size = 0;
@@ -16,11 +17,9 @@ public:
 
 	~Beap() {};
 
-	void siftUp(int startPos, int height);
+	
 
-	void siftDown(int startPos, int endPos, int startHeight);
-
-	int getMinIndexIncontainer(int index1, int index2);
+	int getMaxIndexIncontainer(int index1, int index2);
 
 	std::pair<int, int> search(int value);
 
@@ -29,12 +28,26 @@ public:
 	void push(int value);
 
 	void remove(int value);
+	
+	void printState(const std::string& operation) {
+    	std::cout << "After " << operation << ": ";
+		for (int val : container) {
+			std::cout << val << " ";
+		}
+		std::cout << " | size=" << size << " height=" << height << std::endl;
+	}
+
+private:
+
+	void siftUp(int startPos, int height);
+
+	void siftDown(int startPos, int endPos, int startHeight);
 
 	std::pair<int, int> span(int height);
 
-	std::pair<int, int> getParents(int childHeight, int child);
+	std::pair<int, int> getParents(const int childHeight, const int child);
 
-	std::pair<int, int> getChildren(int parentheight, int index);
+	std::pair<int, int> getChildren(const int parentheight, const int index);
 
 };
 
