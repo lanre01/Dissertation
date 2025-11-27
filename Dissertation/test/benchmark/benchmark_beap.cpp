57ee9e1f1cd7c49d47a4900ab440dcc703b214c3
lawal <alongbijalawal2000@gmail.com>
2025-11-27 23:20:17 +0000

    updated benchmark files

--- Dissertation/test/benchmark/benchmark_beap.cpp
+++ Dissertation/test/benchmark/benchmark_beap.cpp
@@ -12,16 +12,13 @@ std::vector<int> generateRandomData(size_t n) {
     return v;
 }
 
-void SetUp(const ::benchmark::State& s)  {
-        Beap b = Beap();
-}
-
-void TearDown(const ::benchmark::State&) {}
 
 // Benchmark beap construction
-static void BM_Construct(benchmark::State& state) {
+static void BM_Baseline(benchmark::State& state) {
+    size_t count = state.range(0);
     for (auto _ : state) {
         Beap b;
+        b.container.reserve(count);
         benchmark::DoNotOptimize(b);
     }
 }
@@ -32,11 +29,8 @@ static void BM_PushRandom(benchmark::State& state) {
     auto data = generateRandomData(count);
 
     for (auto _ : state) {
-        state.PauseTiming();
         Beap b;
         b.container.reserve(count);
-        state.ResumeTiming();
-
         for (int x : data)
             b.push(x);
     }
@@ -52,9 +46,8 @@ static void BM_PushSortedAsc(benchmark::State& state) {
     std::sort(data.begin(), data.end());
 
     for (auto _ : state) {
-        state.PauseTiming();
         Beap b;
-        state.ResumeTiming();
+        b.container.reserve(count);
 
         for (int x : data)
             b.push(x);
@@ -72,9 +65,8 @@ static void BM_PushSortedDesc(benchmark::State& state) {
     std::sort(data.begin(), data.end(), std::greater<int>());
 
     for (auto _ : state) {
-        state.PauseTiming();
         Beap b;
-        state.ResumeTiming();
+        b.container.reserve(count);
 
         for (int x : data)
             b.push(x);
@@ -87,13 +79,16 @@ static void BM_PushSortedDesc(benchmark::State& state) {
 static void BM_Pop(benchmark::State& state) {
     size_t count = state.range(0);
     auto data = generateRandomData(count);
+    
 
     for (auto _ : state) {
         state.PauseTiming();
         Beap b;
-        for (int x : data) b.push(x);
+        b.container.reserve(count);
+        for(auto d : data)
+            b.push(d);
         state.ResumeTiming();
-
+        
         for (size_t i = 0; i < count; i++) {
             b.pop();
         }
@@ -107,6 +102,7 @@ static void BM_Search(benchmark::State& state) {
     size_t count = state.range(0);
 
     Beap b;
+    b.container.reserve(count);
     auto data = generateRandomData(count);
     for (int x : data) b.push(x);
 
@@ -126,14 +122,12 @@ static void BM_Search(benchmark::State& state) {
 // Benchmark remove(value)
 static void BM_RemoveValue(benchmark::State& state) {
     size_t count = state.range(0);
+    Beap b;
+    b.container.reserve(count);
+    auto data = generateRandomData(count);
+    for (int x : data) b.push(x);
 
     for (auto _ : state) {
-        state.PauseTiming();
-        Beap b;
-        auto data = generateRandomData(count);
-        for (int x : data) b.push(x);
-        state.ResumeTiming();
-
         for (int x : data)
             b.remove(x);
     }
@@ -142,14 +136,18 @@ static void BM_RemoveValue(benchmark::State& state) {
 }
 
 // Benchmark Mixed workload: 40% push, 40% search, 20% pop
-static void BM_MixedWorkload(benchmark::State& state) {
+/*static void BM_MixedWorkload(benchmark::State& state) {
     Beap b;
+    b.container.reserve(state.range(0));
+
 
     std::uniform_int_distribution<int> valdist(1, 1'000'000);
     std::uniform_int_distribution<int> opdist(1, 100);
 
     for (auto _ : state) {
+        state.PauseTiming();
         int op = opdist(rng);
+        state.ResumeTiming();
 
         if (op <= 40) {
             b.push(valdist(rng));
@@ -158,24 +156,24 @@ static void BM_MixedWorkload(benchmark::State& state) {
             benchmark::DoNotOptimize(b.search(valdist(rng)));
         }
         else {
-            if (b.size > 0) b.pop();
+            if (b.size > 0) benchmark::DoNotOptimize(b.pop());
         }
     }
-}
+}*/
 
 
-BENCHMARK(BM_Construct);
+BENCHMARK(BM_Baseline)->RangeMultiplier(4)->Range(256, 1<<20); 
 
-BENCHMARK(BM_PushRandom)->Arg(1000)->Arg(5000)->Arg(10000);
-BENCHMARK(BM_PushSortedAsc)->Arg(1000)->Arg(5000)->Arg(10000);
-BENCHMARK(BM_PushSortedDesc)->Arg(1000)->Arg(5000)->Arg(10000);
+BENCHMARK(BM_PushRandom)->RangeMultiplier(2)->Range(256, 1<<20); 
+BENCHMARK(BM_PushSortedAsc)->RangeMultiplier(2)->Range(256, 1<<20); 
+BENCHMARK(BM_PushSortedDesc)->RangeMultiplier(2)->Range(256, 1<<20); 
 
-BENCHMARK(BM_Pop)->Arg(1000)->Arg(5000)->Arg(10000);
+BENCHMARK(BM_Pop)->RangeMultiplier(2)->Range(256, 1<<20); 
 
-BENCHMARK(BM_Search)->Arg(5000)->Arg(10000);
+BENCHMARK(BM_Search)->RangeMultiplier(4)->Range(256, 1<<18); 
 
-BENCHMARK(BM_RemoveValue)->Arg(2000)->Arg(5000);
+BENCHMARK(BM_RemoveValue)->RangeMultiplier(4)->Range(256, 1<<18); 
 
-BENCHMARK(BM_MixedWorkload)->Arg(50000);
+//BENCHMARK(BM_MixedWorkload)->Arg(1000)->Arg(5000)->Arg(10000);
 
 BENCHMARK_MAIN();
